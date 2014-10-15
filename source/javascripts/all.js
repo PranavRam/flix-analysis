ce4445976aece9804c0bf067b37947b7f6b923e7
Pranav Ram <pranav.ram7@gmail.com>
2014-10-14 22:53:11 -0400

    add force layout

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -8,122 +8,129 @@
 
 $(function() {
     $('.ui.sidebar').sidebar();
+    $('.ui.radio.checkbox').checkbox();
     $('.ui.button').on('click', function() {
         $('.ui.sidebar').sidebar('toggle');
+    });
+    var width = 960,
+        height = 500;
+
+    var color = d3.scale.ordinal().range(['#3d4f53', '#8f4139', '#ab987a']);
+
+    var force = d3.layout.force()
+        .charge(-40)
+        .linkDistance(10)
+        .size([width, height]);
+
+    var svg = d3.select("#chart-container").append("svg")
+        .attr("id", "main-svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    d3.json("data/films.json", function(error, graph) {
+
+        force.nodes(graph.nodes)
+            .links(graph.links)
+            .start();
+
+        var drag = force.drag()
+            .on("dragstart", dragstart);
+
+        var link = svg.selectAll(".link")
+            .data(graph.links)
+            .enter().append("line")
+            .attr("class", "link");
+
+        var node = svg.selectAll("g.node")
+            .data(graph.nodes)
+            .enter().append("svg:g")
+            .attr("class", "node")
+            .on("dblclick", dblclick)
+            .call(force.drag);
+
+        node.filter(function(d) {
+                return d.type == "director"
+            })
+            .append("svg:text")
+            .attr("class", "director nodetext")
+            .attr("dx", 12)
+            .attr("dy", ".35em")
+            .text(function(d) {
+                return d.name
+            });
+
+        node.append("circle")
+            .attr("class", function(d) {
+                return d.type + " nodecircle"
+            })
+            .attr("r", 5)
+            .style("fill", function(d) {
+                return color(d.type);
+            })
+            .on("mouseover", showPopover)
+            .on("mouseout", removePopovers);
+
+        force.on("tick", function() {
+            link.attr("x1", function(d) {
+                    return d.source.x;
+                })
+                .attr("y1", function(d) {
+                    return d.source.y;
+                })
+                .attr("x2", function(d) {
+                    return d.target.x;
+                })
+                .attr("y2", function(d) {
+                    return d.target.y;
+                });
+
+            node.attr("transform", function(d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
+        });
+
+        function dblclick(d) {
+            d3.select(this).classed("fixed", d.fixed = false);
+            // force.start();
+        }
+
+        function dragstart(d) {
+            d3.select(this).classed("fixed", d.fixed = true);
+        }
+    });
+
+    function removePopovers() {
+        $('.popover').each(function() {
+            $(this).remove();
+        });
+    }
+
+    function showPopover(d) {
+        $(this).popup({
+			    content: d.type.slice(0, 1).toUpperCase() + d.type.slice(1) + ": " + d.name
+			  });
+			  $(this).popup('show');
+
+    }
+
+    function resize() {
+        var chart = $("#main-svg");
+        chart.attr("width", chart.parent().width());
+        chart.attr("height", chart.parent().height());
+        force.size([chart.parent().width(), chart.parent().height()]);
+        // force.start();
+    }
+
+    $(document).ready(function() {
+        resize();
+    });
+
+    $(window).on("resize", function() {
+        resize();
     });
 })
 
 angular.module('myApp', ['nvd3'])
-    .controller('myCtrl', function($scope) {
-        $scope.options = {
-            chart: {
-                type: 'lineChart',
-                height: 450,
-                margin: {
-                    top: 20,
-                    right: 20,
-                    bottom: 40,
-                    left: 55
-                },
-                x: function(d) {
-                    return d.x;
-                },
-                y: function(d) {
-                    return d.y;
-                },
-                useInteractiveGuideline: true,
-                dispatch: {
-                    stateChange: function(e) {
-                        console.log("stateChange");
-                    },
-                    changeState: function(e) {
-                        console.log("changeState");
-                    },
-                    tooltipShow: function(e) {
-                        console.log("tooltipShow");
-                    },
-                    tooltipHide: function(e) {
-                        console.log("tooltipHide");
-                    }
-                },
-                xAxis: {
-                    axisLabel: 'Time (ms)'
-                },
-                yAxis: {
-                    axisLabel: 'Voltage (v)',
-                    tickFormat: function(d) {
-                        return d3.format('.02f')(d);
-                    },
-                    axisLabelDistance: 30
-                },
-                callback: function(chart) {
-                    console.log("!!! lineChart callback !!!");
-                }
-            },
-            title: {
-                enable: true,
-                text: 'Title for Line Chart'
-            },
-            subtitle: {
-                enable: true,
-                text: 'Subtitle for simple line chart. Lorem ipsum dolor sit amet, at eam blandit sadipscing, vim adhuc sanctus disputando ex, cu usu affert alienum urbanitas.',
-                css: {
-                    'text-align': 'center',
-                    'margin': '10px 13px 0px 7px'
-                }
-            },
-            caption: {
-                enable: true,
-                html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec<sup>[1, <a href="https://github.com/krispo/angular-nvd3" target="_blank">2</a>, 3]</sup>.',
-                css: {
-                    'text-align': 'justify',
-                    'margin': '10px 13px 0px 7px'
-                }
-            }
-        };
-
-        $scope.data = sinAndCos();
-
-        /*Random Data Generator */
-        function sinAndCos() {
-            var sin = [],
-                sin2 = [],
-                cos = [];
-
-            //Data is represented as an array of {x,y} pairs.
-            for (var i = 0; i < 100; i++) {
-                sin.push({
-                    x: i,
-                    y: Math.sin(i / 10)
-                });
-                sin2.push({
-                    x: i,
-                    y: i % 10 == 5 ? null : Math.sin(i / 10) * 0.25 + 0.5
-                });
-                cos.push({
-                    x: i,
-                    y: .5 * Math.cos(i / 10 + 2) + Math.random() / 10
-                });
-            }
-
-            //Line chart data should be sent as an array of series objects.
-            return [{
-                values: sin, //values - represents the array of {x,y} data points
-                key: 'Sine Wave', //key  - the name of the series.
-                color: '#ff7f0e' //color - optional: choose your own line color.
-            }, {
-                values: cos,
-                key: 'Cosine Wave',
-                color: '#2ca02c'
-            }, {
-                values: sin2,
-                key: 'Another sine wave',
-                color: '#7777ff',
-                area: true //area - set to true if you want this line to turn into a filled area chart.
-            }];
-        };
-
-    })
     .controller('discreteBarChartCtrl', function($scope) {
 
         $scope.options = {
